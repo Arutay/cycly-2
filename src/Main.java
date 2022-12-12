49306554a3bc2cf54deac6e87e8220fe0f9b9d5e
Арутай <adarov.arutay@mail.ru>
2022-12-12 20:33:11 +0700

    ДЗ переделал

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,9 @@ public class Main {
         {
             int contribution = 15_000;
             double total = 0;
+            int l = 2_459_000;
             int month = 0;
-            while (total <= 2_459_000) {
+            while (total <= l) {
                 month = month + 1;
                 total = total + contribution;
                 System.out.println("Месяц " + month + " сумма накоплений равна " + total + " рублей");
@@ -45,10 +46,10 @@ public class Main {
         int birthRate1000 = 17;
         int mortality1000 = 8;
         int size1000 = birthRate1000 - mortality1000;
-        int i = 1;
-        while (i <= 10) {
-            System.out.println("Год " + i + ", численность населения составляет " + population);
-            i = i + 1;
+        int year = 1;
+        while (year <= 10) {
+            System.out.println("Год " + year + ", численность населения составляет " + population);
+            year = year + 1;
             population = population + (population / 1000 * size1000);
         }
     }
@@ -56,60 +57,57 @@ public class Main {
 
     public static void task4() {
         System.out.println("Задача 4");
-        {
             int salary = 15000;
             int total = 0;
-            int i = 0;
-            for (; total < 12_000_000; i++) {
-                total = total + total / 100 * 7;
+            int month = 0;
+            int k = 12_000_000;
+            int p = 7;
+            for (; total < k; month++) {
+                total = total + total / 100 * p;
                 total = total + salary;
-                if (i % 1 == 0) {
-                    System.out.println("Месяц " + i + " Итого " + total);
+                if (month % 1 == 0) {
+                    System.out.println("Месяц " + month + " Итого " + total);
                 }
             }
             System.out.println(total);
         }
-    }
 
 
     public static void task5() {
         System.out.println("Задача 5");
-        {
             int salary = 15000;
             int total = 0;
-            int i = 0;
-            for (; total < 12_000_000; i++) {
-                total = total + total / 100 * 7;
+            int k = 7;
+            int month = 0;
+            int p = 12_000_000;
+            for (; total < p; month++) {
+                total = total + total / 100 * k;
                 total = total + salary;
-                if (i % 6 == 0) {
-                    System.out.println("Месяц " + i + " Итого " + total);
+                if (month % 6 == 0) {
+                    System.out.println("Месяц " + month + " Итого " + total);
                 }
             }
             System.out.println(total);
         }
-    }
 
     public static void task6() {
         System.out.println("Задача 6");
-        {
             int salary = 15000;
             int total = 0;
-            int i = 0;
-            for (; total < 560569; i = i + 6) {
-                total = total + total / 100 * 7;
+            int k = 7;
+            for (int i = 0; i <=108 ; i = i + 6) {
+                total = total + total / 100 * k;
                 total = total + salary;
-                if (i % 6 == 0) {
+                {
                     System.out.println("Месяц " + i + " Итого " + total);
                 }
             }
-        }
         System.out.println();
     }
 
 
     public static void task7() {
         System.out.println("Задача 7");
-        {
             System.out.println();
             for (int k = 1; k <= 31; k++) {
                 if (k % 7 == 0) {
@@ -117,12 +115,13 @@ public class Main {
                 }
             }
         }
-    }
     public static void task8(){
         System.out.println("Задача 8");
-        for (int k = 2022 - 200; k <= 2022 + 100; k++) {
-            if (k % 79 == 0) {
-                System.out.println(k);
+        int l = 200;
+        int p = 100;
+        for (int year = 2022 - l; year <= 2022 + p; year++) {
+            if (year % 79 == 0) {
+                System.out.println(year);
     }
             }
 }}
